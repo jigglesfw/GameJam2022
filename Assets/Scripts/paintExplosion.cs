412f034daf83c407c0382bcaaffd1238dba3e914
k1404635 <soois1114@gmail.com>
2022-09-24 17:52:32 -0500

    ajldsfksajfagadga

---
+++ Assets/Scripts/paintExplosion.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paintExplosion : MonoBehaviour
+{
+    public ParticleSystem particles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(particles.gravityModifier > 0.1f) { particles.gravityModifier -= Time.deltaTime * 5; }
+        
+    }
+
+    
+}
